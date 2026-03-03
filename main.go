da8b8d2aa3ccbc50f139d6c5a153a1f6db406113
micartey <me@micartey.dev>
2026-03-03 20:53:32 +0100

    cleanup

--- main.go
+++ main.go
@@ -5,14 +5,11 @@ import (
 	"time"
 )
 
+// Make sure the baud rate is the same as configured on the LRS Receiver!
 const CRSF_BAUD = 420000
 
-// Channel values extracted to variables outside of main
 var (
-	CH1, CH2, CH3, CH4     uint16
-	CH5, CH6, CH7, CH8     uint16
-	CH9, CH10, CH11, CH12  uint16
-	CH13, CH14, CH15, CH16 uint16
+	CH1, CH2, CH3, CH4, CH5, CH6, CH7, CH8, CH9, CH10, CH11, CH12, CH13, CH14, CH15, CH16 uint16
 
 	Connected     bool
 	LastFrameTime time.Time
@@ -26,6 +23,7 @@ func main() {
 		TX:       machine.GPIO0,
 		RX:       machine.GPIO1,
 	})
+
 	if err != nil {
 		println("UART Config Error:", err.Error())
 	}
@@ -37,14 +35,17 @@ func main() {
 
 	// Main loop can now perform other tasks while channels update in background
 	for {
+		// Check for connection state
+		// this might be important for later when we do something based on the connection state
+		if time.Since(LastFrameTime) > 500*time.Millisecond {
+			Connected = false
+		}
+
 		if !Connected {
 			println("Receiver Disconnected...")
-		} else {
-			if time.Since(LastFrameTime) > 500*time.Millisecond {
-				Connected = false
-			}
 			print("[", CH3, " ", CH4, " - ", CH1, " ", CH2, "] ", CH10, "\r\n")
 		}
+
 		time.Sleep(time.Millisecond * 100)
 	}
 }
@@ -69,9 +70,21 @@ func runReceiver(uart *machine.UART) {
 		LastFrameTime = time.Now()
 
 		// Update global variables
-		CH1, CH2, CH3, CH4 = channels[0], channels[1], channels[2], channels[3]
-		CH5, CH6, CH7, CH8 = channels[4], channels[5], channels[6], channels[7]
-		CH9, CH10, CH11, CH12 = channels[8], channels[9], channels[10], channels[11]
-		CH13, CH14, CH15, CH16 = channels[12], channels[13], channels[14], channels[15]
+		CH1 = channels[0]
+		CH2 = channels[1]
+		CH3 = channels[2]
+		CH4 = channels[3]
+		CH5 = channels[4]
+		CH6 = channels[5]
+		CH7 = channels[6]
+		CH8 = channels[7]
+		CH9 = channels[8]
+		CH10 = channels[9]
+		CH11 = channels[10]
+		CH12 = channels[11]
+		CH13 = channels[12]
+		CH14 = channels[13]
+		CH15 = channels[14]
+		CH16 = channels[15]
 	}
 }
